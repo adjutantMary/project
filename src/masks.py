58d673d2ce1a713d7ff7b07b4e1be90f4260d958
Mariya Polshkova <polshkovamamariya@gmail.com>
2023-11-01 00:27:17 +0300

    Edited formats

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,9 @@
-
 def get_hide(card: str):
     """
     Функция получает номер карты и прячет все символы,
     не включая последние четыре
     """
-    return f'{card[:4]} {card[4:6]}** **** {card[-4:]}'
+    return f"{card[:4]} {card[4:6]}** **** {card[-4:]}"
 
 
 def get_bill(bill: str):
@@ -13,8 +12,8 @@ def get_bill(bill: str):
     только последние четыре знака.
     Остальные скрыты за '**'
     """
-    return f'**{bill[-4:]}'
+    return f"**{bill[-4:]}"
 
 
-print(get_hide('8990922113665229'))
-print(get_bill('8990922113665229'))
+print(get_hide("8990922113665229"))
+print(get_bill("8990922113665229"))
--- src/widget.py
+++ src/widget.py
@@ -2,11 +2,11 @@ from src.masks import get_hide, get_bill
 
 
 def get_hided(bill_number: str):
-    '''
+    """
     Принимает на вход строку информацией тип карты/счета и номер карты/счета
     Возвращает эту строку с замаскированным номером карты/счета
-    '''
-    splitted_number = bill_number.split(' ')  # ['Visa', 'Platinum', '888']
+    """
+    splitted_number = bill_number.split(" ")  # ['Visa', 'Platinum', '888']
     alpha_list = []
     digit_list = []
     for symbol in splitted_number:
@@ -15,21 +15,20 @@ def get_hided(bill_number: str):
         else:
             digit_list.append(symbol)
 
-    if 'Счет' in alpha_list:
-        return f'Счет **{digit_list[-4:]}'
+    if "Счет" in alpha_list:
+        return f"Счет **{get_bill(digit_list[0])}"
     else:
-        return f'{" ".join(alpha_list)} {digit_list[0][:-12]} {digit_list[0][-12:-10]}** **** {digit_list[0][-4:]}'
+        return f'{" ".join(alpha_list)} {get_hide(digit_list[0])}'
 
 
 def get_data(data: str):
-    '''
+    """
     Принимает строку, возвращая измененную строку в формате даты
-    '''
+    """
 
-    data_string = data[0:9].split('-')
-    return f'{data_string[2]}.{data_string[1]}.{data_string[0]}'
+    data_string = data[0:10].split("-")
+    return f"{data_string[2]}.{data_string[1]}.{data_string[0]}"
 
 
-print(get_hided('Visa Platinum 8990922113665229'))
-print(get_data('2018-07-11T02:26:18.671407'))
-
+print(get_hided("Visa Platinum 8990922113665229"))
+print(get_data("2018-07-11T02:26:18.671407"))
