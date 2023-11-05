9bd7f136517f03620b675217d14c3f3ff9c5eb07
Mariya Polshkova <polshkovamamariya@gmail.com>
2023-11-05 20:55:19 +0300

    Test masks.py

---
+++ tests2/test_masks.py
@@ -0,0 +1,14 @@
+import pytest
+from src.masks import get_hide, get_bill
+
+@pytest.fixture
+def num():
+    return '8990922113665229'
+
+
+def test_masks_correct_work(num):
+    assert get_bill(num) == '**5229'
+    assert get_hide(num) == '8990 92** **** 5229'
+    assert type(get_bill(num)) == str
+    assert type(get_hide(num)) == str
+
