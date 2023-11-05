cdb16500879abf2a2fc58d2241d103e529297f89
Mariya Polshkova <polshkovamamariya@gmail.com>
2023-11-05 20:12:23 +0300

    Test processing.py

---
+++ tests2/test_processing.py
@@ -0,0 +1,32 @@
+import pytest
+from src.processing import get_sorted, get_state
+
+@pytest.fixture
+def type_word():
+    return 'EXECUTED'
+
+@pytest.fixture
+def type_sort():
+    return 'High'
+
+@pytest.fixture
+def list_in_dict():
+    return [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}]
+
+
+def test_get_state_correct_work(list_in_dict, type_word):
+    assert get_state(type_word, list_in_dict) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                                                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+                                                  ]
+    assert type(get_state(type_word, list_in_dict)) == list
+
+
+def test_get_sorted_correct_work(list_in_dict, type_sort):
+    assert get_sorted(list_in_dict, type_sort) == [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                                                   {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                                                   {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                                                   {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
+    assert type(get_sorted(list_in_dict, type_sort)) == list
