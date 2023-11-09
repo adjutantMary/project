9272f1efc8c8b22dbbb9121f982e415a6f7b120e
Mariya Polshkova <polshkovamamariya@gmail.com>
2023-11-09 22:22:01 +0300

    Edited by black

--- tests2/test_generators.py
+++ tests2/test_generators.py
@@ -1,6 +1,7 @@
 import pytest
 from src.generators import usd_generators, description_translator, get_cards_number
 
+
 @pytest.fixture
 def list_group():
     return [
@@ -14,37 +15,31 @@ def list_group():
             "to": "Счет 11776614605963066702",
         }
     ]
+
+
 @pytest.fixture
 def type_code():
-    return 'USD'
+    return "USD"
 
 
 def test_usd(list_group, type_code):
     check_usd = list(usd_generators(list_group, type_code))
     for line in check_usd:
-        if line['id']:
-            assert line['id'] == 939719570
+        if line["id"]:
+            assert line["id"] == 939719570
 
 
 def test_translator(list_group):
     check_translation = list(description_translator(list_group))
-    # for line in check_translation:
-    #     if line['description']:
-    assert check_translation == ['Перевод организации']
+    assert check_translation == ["Перевод организации"]
 
 
 def test_cards_number():
     check_translation = list(get_cards_number(1, 5))
-    assert check_translation == ['0000 0000 0000 0001',
-                                 '0000 0000 0000 0002',
-                                 '0000 0000 0000 0003',
-                                 '0000 0000 0000 0004',
-                                 '0000 0000 0000 0005'
-                                 ]
-
-
-
-
-
-
-
+    assert check_translation == [
+        "0000 0000 0000 0001",
+        "0000 0000 0000 0002",
+        "0000 0000 0000 0003",
+        "0000 0000 0000 0004",
+        "0000 0000 0000 0005",
+    ]
