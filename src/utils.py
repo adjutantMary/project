869ea802e913d2716d4fc3baad47dc0a0d77f647
Mariya Polshkova <polshkovamamariya@gmail.com>
2023-11-23 13:10:35 +0300

    Loggers for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,18 @@
 import json
+import logging
+import os
 from json import JSONDecodeError
 from typing import Any
+
+
+logger = logging.getLogger(__name__)
+if os.path.exists("utils.log"):
+    os.unlink("utils.log")
+file_handler = logging.FileHandler("utils.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_json_file(path: str) -> list[dict[Any, Any]]:
@@ -10,15 +22,17 @@ def get_json_file(path: str) -> list[dict[Any, Any]]:
     :param path: Путь к файлу типа str
     :return: list[dict]
     """
+    file_logger = logging.getLogger(__name__)
     try:
         with open(path, encoding="UTF-8") as file:
             json_dict = json.load(file)
+            file_logger.info("Выгрузка JSON - словаря")
             return json_dict
     except JSONDecodeError:
-        print("JSON-файл имеет неправильный формат ")
+        file_logger.error("JSONDecodeError: JSON-файл имеет неправильный формат")
         return []
     except FileNotFoundError:
-        print("Файл не найден")
+        file_logger.error("FileNotFoundError: Файл не найден")
         return []
     except FileExistsError:
         print("Файл не существует")
@@ -31,8 +45,11 @@ def get_transactions(transaction: dict) -> float:
     :param transaction: Принимает транзакцию типа dict
     :return:
     """
+    transaction_logger = logging.getLogger(__name__)
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
+        transaction_logger.info(f'Номер транзацкции: {float(transaction["operationAmount"]["amount"])}')
         return float(transaction["operationAmount"]["amount"])
 
     else:
+        transaction_logger.error("ValueError: Транзация выполнена не в рублях. Укажите транзакцию в рублях")
         raise ValueError("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
